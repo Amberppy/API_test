543fd8d4b86d9fdc02eee38bcd3cc6caca25a345
bearppy <187396693@qq.com>
2020-02-24 15:30:02 +0800

    '第二次提交'

--- test.py
+++ test.py
@@ -4,10 +4,44 @@
 @Date  : 2020/2/22 15:19
 @Desc  : 
 '''
-import json
 
-str = {"code": "100000","msg": "成功","data": {"name": "范冰冰","password": "e10adc3949ba59abbe56e057f20f883e"}}
-str_js=json.dumps(str)
-print(str_js)
-result = json.loads(str_js, encoding='utf-8')
-print(type(result))
+from flask import Flask,request,jsonify,abort
+import random
+
+app = Flask(__name__)  #实例化一个Flask对象
+
+@app.route("/api/user/reg",methods=['POST'])
+
+def reg():
+    if not request.json or "name" not in request.json or "password" not in request.json:
+        abort(404)
+    res = [
+        {
+            "code": "100000",
+            "msg": "成功",
+            "data": {
+                "name": "李六",
+                "password": "e10adc3949ba59abbe56e057f20f883e"
+            }
+        },
+        {
+            "code": "100001",
+            "msg": "失败，用户已存在",
+            "data": {
+                "name": "李六",
+                "password": "e10adc3949ba59abbe56e057f20f883e"
+            }
+        },
+        {
+            "code": "100002",
+            "msg": "失败，添加用户失败",
+            "data": {
+                "name": "李六",
+                "password": "e10adc3949ba59abbe56e057f20f883e"
+            }
+        }
+    ]
+    return jsonify(random.choice(res))
+
+if __name__=='__main__':
+    app.run()
